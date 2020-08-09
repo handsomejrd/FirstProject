cc8d09b929bb39406943d56cc02251dfa6847ced
danny <369742195@qq.com>
2020-08-09 12:03:15 +0800

    first

--- src/com/firtprojeck/one/MainClass.java
+++ src/com/firtprojeck/one/MainClass.java
@@ -3,5 +3,6 @@ package com.firtprojeck.one;
 public class MainClass {
     public static void main(String args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
